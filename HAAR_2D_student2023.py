17cf228d7f3f7309db3e1b59b171ad7b3d456734
ouaburst <45198357+ouaburst@users.noreply.github.com>
2023-02-13 23:56:21 +0100

    Update HAAR_2D_student2023.py

--- HAAR_2D_student2023.py
+++ HAAR_2D_student2023.py
@@ -6,11 +6,11 @@ Created on Mon Jan 30 13:25:08 2023
 """
 # fil för datainläsning 
 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
 #import cupy as cp
-import os
-from mpl_toolkits.mplot3d import Axes3D
+#import os
+#from mpl_toolkits.mplot3d import Axes3D
 from PIL import Image
 from matplotlib import pyplot
 #from keras.datasets import mnist
@@ -33,45 +33,76 @@ X=Xfull[0:datasize,0:datasize]
 #X=[[9,7,6,2],[5,3,4,4],[8,2,4,0],[6,0,2,2]]
 #pyplot.imshow(X, cmap=pyplot.get_cmap('gray'))
 
-def Wavelet_matrix(data, wavelet_type='haar'):
-    """
-    Calculates h0 and s0 using the 2D discrete wavelet transform (DWT).
-    
-    data: 2D matrix containing the image data
-    wavelet_type: type of wavelet to be used for the DWT (default is 'haar')
-    
-    Returns: h0, s0 (the horizontal and vertical detail coefficients)
-    """
-    # Perform 2D DWT on the image data
-    coeffs = pywt.dwt2(data, wavelet_type)
+# --- Haar matrix ---
 
-    # Get the horizontal and vertical detail coefficients (h0 and s0)
-    cA, (cH, cV, cD) = coeffs
-    
-    # h0 is the horizontal detail coefficients
-    h0 = cH
+def haar_matrix(n):
+    # Initialize row counter
+    m = 0
+    # Check if n is even
+    if n % 2 != 0:
+        raise ValueError("n must be an even number")
+    # Create a zero matrix of size n x n using NumPy
+    matrix = np.zeros((n, n))
+    # Loop over the first half of the rows
+    for i in range(n // 2):            
+        # Set values in the matrix according to the Haar matrix formula
+        matrix[m, i] = 0.5        
+        matrix[m+1, i] = 0.5
+        matrix[m, i + n // 2] = 0.5        
+        matrix[m+1, i + n // 2] = -0.5
+        # Increment the row counter by 2
+        m += 2
+    # Return the resulting matrix
+    return matrix
 
-    # s0 is the vertical detail coefficients
-    s0 = cV
-    
-    return h0, s0
+# --- Compress the image ---
 
-#def matrix_multiplication(A, B):
-#    return np.matmul(A, B)
+h0 = haar_matrix(4096)
+s0 = data
 
-# Calculate h0 and s0 using the Wavelet_matrix function
-h0, s0 = Wavelet_matrix(data,'haar')
+# Horizontal matrix multiplication 
+# s0.h0
 
-print ('h0', h0)
-print ('s0', s0)
+print("----- horizontal_matrix_mult -----")
+horizontal_matrix_mult = np.matmul(s0, h0)
+print(horizontal_matrix_mult)
 
-# Matrix multiplication horizontally
-#print("----- horizontal_matrix_mult -----")
-#horizontal_matrix_mult = np.matmul(s0, h0)
-#print(horizontal_matrix_mult)
+pyplot.imshow(horizontal_matrix_mult, cmap=pyplot.get_cmap('gray'))
 
-# Wavelet transformation for 2D matrix Eq7.5
-transform_matrix = np.matmul(np.transpose(h0),np.matmul(s0,h0))
-print(transform_matrix)
 
-pyplot.imshow(transform_matrix, cmap=pyplot.get_cmap('gray'))
+# Vertical matrix multiplication
+# transpose(h0).(s0.h0)
+
+print("----- vertical_matrix_mult -----")
+vertical_matrix_mult = np.matmul(np.transpose(h0), horizontal_matrix_mult)
+print(vertical_matrix_mult)
+
+pyplot.imshow(vertical_matrix_mult, cmap=pyplot.get_cmap('gray'))
+
+
+# --- Recontruct the image
+
+# --- Compute the inverse Haar matrix ---
+
+datasize = 4096
+inv_h0 = np.transpose(h0) / (datasize//2)
+
+# --- Decompress the image ---
+
+# Vertical matrix multiplication
+# inv_h0.vertical_matrix_mult
+
+print("----- vertical_matrix_mult_inv -----")
+vertical_matrix_mult_inv = np.matmul(inv_h0, vertical_matrix_mult)
+print(vertical_matrix_mult_inv)
+
+pyplot.imshow(vertical_matrix_mult_inv, cmap=pyplot.get_cmap('gray'))
+
+# Horizontal matrix multiplication
+# vertical_matrix_mult_inv.inv_h0
+
+print("----- horizontal_matrix_mult_inv -----")
+horizontal_matrix_mult_inv = np.matmul(vertical_matrix_mult_inv, inv_h0)
+print(horizontal_matrix_mult_inv)
+
+pyplot.imshow(horizontal_matrix_mult_inv, cmap=pyplot.get_cmap('gray'))
